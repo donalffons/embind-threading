7f4201355e8bb93f8be72b582da417e70768d346
Sebastian Alff <donalffons@gmail.com>
2023-02-06 23:38:30 +0100

    improved structure

--- packages/node-project/index.js
+++ packages/node-project/index.js
@@ -1,23 +1,35 @@
 import init from "emscripten-project/dist/TestApplication.js";
 
+const loadThread = (Module, thread, url) => new Promise(res => {
+  Atomics.waitAsync(Module.HEAP32, thread.getAliveAddress() / 4, 0).value.then(res);
+  thread.load(url);
+});
+
+const runThreadFunction = (Module, thread, name) => new Promise(res => {
+  Atomics.waitAsync(Module.HEAP32, thread.getAliveAddress() / 4, 1).value.then(res);
+  thread.run(name);
+});
+
+const killThread = (Module, thread) => new Promise(res => {
+  Atomics.waitAsync(Module.HEAP32, thread.getAliveAddress() / 4, 1).value.then(res);
+  thread.kill();
+});
+
+
 init().then(async Module => {
   console.log(Date.now(), "Spawning thread");
   const myThread = new Module.MyThread();
-  Atomics.waitAsync(Module.HEAP32, myThread.getAliveAddress() / 4, 0).value.then(async () => {
-    console.log(Date.now(), "after load");
-    await new Promise(res => setTimeout(res, 1000));
 
-    Atomics.waitAsync(Module.HEAP32, myThread.getAliveAddress() / 4, 1).value.then(async () => {
-      console.log(Date.now(), "after run");
-      await new Promise(res => setTimeout(res, 1000));
+  await loadThread(Module, myThread, "thread.js");
+  console.log(Date.now(), "after load");
 
-      Atomics.waitAsync(Module.HEAP32, myThread.getAliveAddress() / 4, 1).value.then(async () => {
-        console.log(Date.now(), "after kill. bye.");
-      });
-      myThread.kill();
-      await new Promise(res => setTimeout(res, 5000));
-    });
-    myThread.run("myThreadFunction");
-  });
-  myThread.load("thread.js");
+  await new Promise(res => setTimeout(res, 1000));
+
+  await runThreadFunction(Module, myThread, "myThreadFunction");
+  console.log(Date.now(), "after run");
+
+  await new Promise(res => setTimeout(res, 1000));
+
+  await killThread(Module, myThread);
+  console.log(Date.now(), "after kill. bye.");
 });
